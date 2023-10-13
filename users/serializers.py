bcbeaad453796c7cdcbdbc9c14337686999f9324
BasilZap <zapr9@yandex.ru>
2023-10-13 04:22:31 +0300

    Расширен сериализатор для профиля пользователя.

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
+from univer.serializers import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentsSerializer(source='payments_set', many=True)
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'payments', 'first_name', 'last_name', 'email', 'phone', 'city', 'avatar')
+
