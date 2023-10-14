1871b7fa787250c48684e822f84bbc009c4a212f
BasilZap <zapr9@yandex.ru>
2023-10-14 23:22:04 +0300

    Курсы и уроки прописаны в панель администратора

--- univer/admin.py
+++ univer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from univer.models import Lesson, Course
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course', 'description', 'image', 'link', 'owner')
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'owner')
