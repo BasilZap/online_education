d3443af0ad92aa182204e076638b79e12dd5b09b
BasilZap <zapr9@yandex.ru>
2023-10-22 23:50:53 +0300

    Изменение модели Payments

--- univer/models.py
+++ univer/models.py
@@ -7,6 +7,10 @@ PAY_METHODS = (
     ('cash', 'наличные'),
     ('transfer', 'безналичный расчет')
 )
+STATUSES = (
+    ('success', 'оплачено'),
+    ('not_paid', 'не оплачено')
+)
 
 
 # Модель 'Курс'
@@ -44,11 +48,15 @@ class Lesson(models.Model):
 # Модель Платежи
 class Payments(models.Model):
     pay_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    pay_date = models.DateField(verbose_name='Дата оплаты')
+    pay_date = models.DateField(auto_now_add=True, verbose_name='Дата оплаты')
     payd_course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE)
     payd_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE)
-    pay_amount = models.FloatField(verbose_name='Сумма оплаты', **NULLABLE)
+    pay_amount = models.IntegerField(verbose_name='Сумма оплаты', **NULLABLE)
     pay_method = models.CharField(max_length=10, choices=PAY_METHODS, verbose_name='Способ оплаты')
+
+    pay_id = models.CharField(max_length=100, default='', verbose_name='ID оплаты', **NULLABLE)
+    pay_status = models.CharField(max_length=15, choices=STATUSES, default='not_paid', verbose_name='Статус оплаты',
+                                  **NULLABLE)
 
     def __str__(self):
         return f'{self.pay_user} {self.payd_course if self.payd_course else self.payd_lesson} ' \
