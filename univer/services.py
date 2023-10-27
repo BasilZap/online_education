473b7146675035907086b0737aeb34ff0ce0bafa
BasilZap <zapr9@yandex.ru>
2023-10-27 22:56:29 +0300

    Добавлена функция send_mail_to_user для отправки email

--- univer/services.py
+++ univer/services.py
@@ -1,6 +1,9 @@
-import stripe
+import smtplib
 
-from config.settings import STRIPE_API_KEY
+import stripe
+from django.core.mail import send_mail
+
+from config.settings import STRIPE_API_KEY, EMAIL_HOST_USER
 
 stripe.api_key = STRIPE_API_KEY
 
@@ -37,3 +40,22 @@ def stripe_get_success(pay_id):
         pay_id,
     )
     return pay_state.status
+
+
+def send_mail_to_user(user_mail: str, message_title: str, user_message: str):
+    """
+    Отправка email пользователю
+    :param user_mail: почтовый адрес -> str
+    :param message_title: заголовок сообщения -> str
+    :param user_message: сообщение пользователю -> str
+    :return: None
+    """
+    try:
+        send_mail(
+            subject=message_title,
+            message=user_message,
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user_mail]
+        )  # Отправляем email с указанным сообщением
+    except smtplib.SMTPException as mail_error:
+        print(mail_error)
